6078736ffd5541f967d8b916cd0570ac0b596af3
Ferdinand Malcher <ferdinand@malcher.media>
2023-03-29 10:01:54 +0200

    Dashboard angelegt

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Book Rating';
+
+  constructor() {
+    setTimeout(() => {
+      this.title = 'dhfuksdhfdk';
+    }, 2000);
+  }
+}
--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CartDisplayComponent } from './cart-display/cart-display.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CartDisplayComponent
+    CartDisplayComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
